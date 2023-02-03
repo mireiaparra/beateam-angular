d69ad93db6ba7ac8b5daaed6d398a3dea191d1c3
Mireia Maldonado Parra <mirmalparra@gmail.com>
2023-02-03 11:03:31 +0100

    Create environment

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    urlAPI: 'https://www.azurglobal.es/apiPracticas/index.php'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    urlAPI: 'https://www.azurglobal.es/apiPracticas/index.php'
+};
